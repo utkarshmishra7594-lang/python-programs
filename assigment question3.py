0df83c5e878caac67e88f08324647f672c38cd8d
utkarshmishra7594-lang <utkarshmishra7594@gmail.com>
2025-09-17 18:26:43 +0530

    Update assigment question3.py

--- assigment question3.py
+++ assigment question3.py
@@ -1,11 +1,8 @@
-'''
 n=input("enter the number:")
 sum=0
 for i in n:
     sum=int(i)+sum
-print("sum of digit=",sum)'''  
+print("sum of digit=",sum)  
 
-with open("demo.txt","w+") as f:
-    f.write("hi\n")
-    f.seek(0)
-    print(f.read())
+
+
