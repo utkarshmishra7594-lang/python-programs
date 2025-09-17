d8a2c731065c48915d2773ffd6fad758ed94303a
utkarshmishra7594-lang <utkarshmishra7594@gmail.com>
2025-09-17 18:23:37 +0530

    Update assigment question1.py

--- assigment question1.py
+++ assigment question1.py
@@ -1,15 +1,14 @@
-num1=int(input("enter first number :"))#x=number1
-num2=int(input("enter second number :"))#y=number2
+num1=int(input("enter first number :"))
+num2=int(input("enter second number :"))
 if num2>num1:
     smallar=num1
-    
 else:
     smallar=num2
     
-
 for i in range(1,smallar+1):
     if((num1%i==0) and (num2%i==0)):
         GCD=i     
         
 print("GCD is=",GCD)
+
 
